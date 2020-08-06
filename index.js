5f3d5e6d9cd70811a635383c982c817ca9390aba
Dhruv Patel <druv5319@gmail.com>
2020-08-05 21:34:40 -0400

    Removed Packages

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
-const cors = require("cors");
 require('./routes/sneaks.routes.js')(app);
 const SneaksAPI = require('./controllers/sneaks.controllers.js');
 
