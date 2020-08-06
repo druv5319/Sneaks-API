343b4ccbb4d63687708484aac634ae54d0d56a3d
Dhruv Patel <druv5319@gmail.com>
2020-08-06 15:10:47 -0400

    setting up db

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 require('./routes/sneaks.routes.js')(app);
+require('dotenv').config();
 const SneaksAPI = require('./controllers/sneaks.controllers.js');
-var port = process.env.PORT || 3000;
 
+var port = process.env.PORT || 3000;
 mongoose.Promise = global.Promise;
 /*Sneaker.deleteMany({ }, function (err) {
   if(err) console.log(err);
@@ -13,12 +14,7 @@ mongoose.Promise = global.Promise;
 });*/
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {}).then(() => {
-  console.log("Successfully connected to the database");
-}).catch(err => {
-  console.log('Could not connect to the database', err);
-  process.exit();
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sneakers');
 
 app.listen(port, function () {
   console.log(`Sneaks app listening on port `, port);
