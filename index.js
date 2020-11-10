27d0bbd4bc1c3b8ab78b28abd6b0e0e528f3c717
Dhruv Patel <druv5319@gmail.com>
2020-11-10 18:31:11 -0500

    debug

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.Promise = global.Promise;
 
 // Connecting to the database
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sneakers');
-
+console.log("IM HEREEREREREJLSKFJKLSDJ@(*(&(*$");
 app.listen(port, function () {
   console.log(`Sneaks app listening on port `, port);
  });
