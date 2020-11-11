1ec1ed22cc68b7bcab38d311495b8f79afe61434
Dhruv Patel <druv5319@gmail.com>
2020-11-10 19:02:55 -0500

    debug

--- scrapers/goat-scraper.js
+++ scrapers/goat-scraper.js
@@ -37,7 +37,7 @@ module.exports = {
       try {
         const response = await got(apiLink, {
           headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
             'Content-Type': 'application/json'
           },
           http2: true,
