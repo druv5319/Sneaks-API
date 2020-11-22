9be712642d0e10bc6969ce0823528b147469b05d
Dhruv Patel <druv5319@gmail.com>
2020-11-22 17:31:58 -0500

    Update user-agent

--- scrapers/goat-scraper.js
+++ scrapers/goat-scraper.js
@@ -39,7 +39,7 @@ module.exports = {
       try {
         const response = await got(apiLink, {	
           headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; rv:20.0) Gecko/20121202 Firefox/20.0',
             'Content-Type': 'application/json',
           },
           
@@ -76,7 +76,7 @@ module.exports = {
       try {
         const response = await got(apiLink, {
           headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; rv:20.0) Gecko/20121202 Firefox/20.0',
             'Content-Type': 'application/json',
           },
           http2: true,
